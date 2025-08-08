e4766637b2b3c6440bb377b249c84731d76b9925
patricia_xiao <patricia.xiao@gmail.com>
2025-08-08 00:33:54 -0500

    Update default.js

--- MyNote/config/default.js
+++ MyNote/config/default.js
@@ -4,7 +4,7 @@ window.config = {
     dicomWeb: [
       {
         name: 'Orthanc',
-        wadoUriRoot: 'http://172.28.119.119:8042/wado',
+        wadoUriRoot: 'http://172.28.119.119:8042/dicom-web',
         qidoRoot: 'http://172.28.119.119:8042/dicom-web',
         wadoRoot: 'http://172.28.119.119:8042/dicom-web',
         qidoSupportsIncludeField: true,
