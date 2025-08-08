c0c0323ca6663952c309a6892107317844136dc6
patricia_xiao <patricia.xiao@gmail.com>
2025-08-08 00:54:24 -0500

    Update local_orthanc.js

--- MyNote/config/local_orthanc.js
+++ MyNote/config/local_orthanc.js
@@ -18,9 +18,9 @@ window.config = {
       configuration: {
         friendlyName: 'local Orthanc DICOMWeb Server',
         name: 'DCM4CHEE',
-        wadoUriRoot: 'http://localhost:8042/dicom-web',
-        qidoRoot: 'http://localhost:8042/dicom-web',
-        wadoRoot: 'http://localhost:8042/dicom-web',
+        wadoUriRoot: 'http://host.docker.internal:8042/dicom-web',
+        qidoRoot: 'http://host.docker.internal:8042/dicom-web',
+        wadoRoot: 'http://host.docker.internal:8042/dicom-web',
         qidoSupportsIncludeField: true,
         supportsReject: true,
         dicomUploadEnabled: true,
@@ -37,12 +37,15 @@ window.config = {
           // just use the correct prefix out of the box, but that is sometimes hard
           // when URLs go through several systems.
           // Example URLS are:
-          // "BulkDataURI" : "http://localhost:8042/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011",
+          // "BulkDataURI" : "http://host.docker.internal:8042/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011",
           // when running on http://localhost:3003 with no server running on localhost.  This can be corrected to:
           // /orthanc/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011
           // which is a valid relative URL, and will result in using the http://localhost:3003/orthanc/.... path
           // startsWith: 'http://localhost/',
           // prefixWith: '/orthanc/',
+        },
+        requestOptions: {
+          auth: 'orthanc:orthanc',
         },
       },
     },
