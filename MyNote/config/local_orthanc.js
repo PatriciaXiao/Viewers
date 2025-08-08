923b7a5d31615c8e349923e985fb09cf33442232
patricia_xiao <patricia.xiao@gmail.com>
2025-08-08 02:34:42 -0500

    correction

--- MyNote/config/local_orthanc.js
+++ MyNote/config/local_orthanc.js
@@ -18,9 +18,9 @@ window.config = {
       configuration: {
         friendlyName: 'local Orthanc DICOMWeb Server',
         name: 'DCM4CHEE',
-        wadoUriRoot: 'http://host.docker.internal:8042/dicom-web',
-        qidoRoot: 'http://host.docker.internal:8042/dicom-web',
-        wadoRoot: 'http://host.docker.internal:8042/dicom-web',
+        wadoUriRoot: 'http://172.28.119.119:8042/dicom-web',
+        qidoRoot: 'http://172.28.119.119:8042/dicom-web',
+        wadoRoot: 'http://172.28.119.119:8042/dicom-web',
         authorization: 'Basic ' + btoa('PatriciaXiao:patxiao2025'),
         qidoSupportsIncludeField: true,
         supportsReject: true,
@@ -38,7 +38,7 @@ window.config = {
           // just use the correct prefix out of the box, but that is sometimes hard
           // when URLs go through several systems.
           // Example URLS are:
-          // "BulkDataURI" : "http://host.docker.internal:8042/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011",
+          // "BulkDataURI" : "http://172.28.119.119:8042/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011",
           // when running on http://localhost:3003 with no server running on localhost.  This can be corrected to:
           // /orthanc/dicom-web/studies/1.2.276.0.7230010.3.1.2.2344313775.14992.1458058363.6979/series/1.2.276.0.7230010.3.1.3.1901948703.36080.1484835349.617/instances/1.2.276.0.7230010.3.1.4.1901948703.36080.1484835349.618/bulk/00420011
           // which is a valid relative URL, and will result in using the http://localhost:3003/orthanc/.... path
